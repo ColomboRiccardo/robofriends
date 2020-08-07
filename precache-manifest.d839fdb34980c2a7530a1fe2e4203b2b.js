7ebdc8c1d9f2aa0d877a8666691435d6fd06d6dd
Riccardo Colombo <colombo.riccardo@protonmail.com>
2020-08-07 19:25:15 +0200

    Updates

---
+++ precache-manifest.d839fdb34980c2a7530a1fe2e4203b2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f52b6551606684bfd9e637bb61f039",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "9624321ae334b7bb6105",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3fd379eef530074aa135",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9624321ae334b7bb6105",
+    "url": "/robofriends/static/js/2.2ef5f8f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.2ef5f8f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fd379eef530074aa135",
+    "url": "/robofriends/static/js/main.cb96304a.chunk.js"
+  },
+  {
+    "revision": "d4d7c4d9f3f71bdf876b",
+    "url": "/robofriends/static/js/runtime-main.6663f93a.js"
+  }
+]);
